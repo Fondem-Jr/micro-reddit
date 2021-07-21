61364f07fd8ec33a4eff47fd4820321e1f6ec474
Fondem Junior <fondemjunior@yahoo.com>
2021-07-21 22:32:50 +0100

    Create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, :presence => true, :uniqueness => true, :length => { :in => 4..20 }
+  validates :email, :presence => true, :uniqueness => true
+  validates :password, :confirmation => true #password_confirmation attr
+  validates_length_of :password, :in => 6..16, :on => :create
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_20_225854) do
+ActiveRecord::Schema.define(version: 2021_07_21_205036) do
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
     t.string "email"
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
