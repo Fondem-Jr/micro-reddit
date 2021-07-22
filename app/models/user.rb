2a9f4bf05a4f4432d00c07dc2710f6da817058a8
Fondem Junior <fondemjunior@yahoo.com>
2021-07-22 22:18:53 +0100

    Add comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
   has_many :posts
+
   validates :username, :presence => true, :uniqueness => true, :length => { :in => 4..20 }
   validates :email, :presence => true, :uniqueness => true
   validates :password, :confirmation => true #password_confirmation attr
