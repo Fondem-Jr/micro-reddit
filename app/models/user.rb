c1a383da271cd977f127a1bca012b280f94e0e6b
Fondem Junior <fondemjunior@yahoo.com>
2021-07-22 23:03:58 +0100

    Add comment model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
 
   has_many :posts
-
+  has_many :comments
+  
   validates :username, :presence => true, :uniqueness => true, :length => { :in => 4..20 }
   validates :email, :presence => true, :uniqueness => true
   validates :password, :confirmation => true #password_confirmation attr
